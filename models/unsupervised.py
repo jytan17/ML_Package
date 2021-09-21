70655d70bada7d18e5983ef0ceabda383e24b002
jytan17 <87595774+jytan17@users.noreply.github.com>
2021-09-21 16:42:13 -0700

    Update unsupervised.py

--- models/unsupervised.py
+++ models/unsupervised.py
@@ -1,1 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+
+###################################
+# Clustering
+###################################
+  
+  
+class kMeans():
+  def __init__(self):
+    pass
+  
+class GaussianMixture():
+  def __init__(self):
+    pass
+
+  
+  
+###################################
+# Dimensionality Reduction
+###################################
+
+class PCA():
+  def __init__(self):
+    pass
+  
+  
