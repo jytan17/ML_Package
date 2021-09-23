dade0bf1f5b378337a0f9abe8a47eb1793d7636e
jytan17 <87595774+jytan17@users.noreply.github.com>
2021-09-22 23:58:10 -0700

    Update supervised.py

--- models/supervised.py
+++ models/supervised.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.preprocessing
+from cvxpy import *
 
 ##############################################################
 # Regression
