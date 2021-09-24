1b590accbddcbe6d71916f2938270684dc268997
jytan17 <87595774+jytan17@users.noreply.github.com>
2021-09-24 12:21:13 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='models',
+    url='https://github.com/jytan17/ML_Package',
+    author='Junyong Tan',
+    author_email='jtan9801@gmail.com',
+    # Needed to actually package something
+    packages=['models'] #, 'models.supervised', 'models.unsupervised'],
+    # Needed for dependencies
+    install_requires=['numpy', 'matplotlib', 'sklearn', 'cvxpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='None',
+    description='A small package of commonly used machine learning algorithms'
+)
