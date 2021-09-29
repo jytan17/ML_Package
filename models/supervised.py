95a105ad3c19304e00dee277a2243e3aa2be765d
jytan17 <87595774+jytan17@users.noreply.github.com>
2021-09-29 00:49:12 -0700

    Update supervised.py

--- models/supervised.py
+++ models/supervised.py
@@ -4,90 +4,6 @@ import sklearn.preprocessing
 from cvxpy import quad_form, multiply, Variable, Problem, Minimize, pos, norm
 import random
 
-class FeedForward():
-
-    def __init__(self, sizes):
-        self.num_layers = len(sizes)
-        self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-
-    def sigmoid(self, z):
-        return 1.0/(1.0+np.exp(-z))
-
-    def sigmoid_prime(self, z):
-        return self.sigmoid(z)*(1-self.sigmoid(z))
-
-    def feedforward(self, a):
-        for b, w in zip(self.biases, self.weights):
-            a = self.sigmoid(np.dot(w, a)+b)
-        return a
-
-    def fit(self, Xtr, ytr, epochs, mini_batch_size, eta, Xva = None, yva = None):
-        training_data = [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(Xtr, ytr)]
-
-        n = len(training_data)
-        for j in range(epochs):
-            random.shuffle(training_data)
-            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            if Xva is not None and yva is not None:
-                validation_data = [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(Xva, yva)]
-                n_test = len(validation_data)
-                print("Epoch {0} validation acc. {1} / {2}".format(j, self.evaluate(validation_data), n_test))
-            else:
-                print("Epoch {0} complete".format(j))
-
-    def update_mini_batch(self, mini_batch, eta):
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-
-        self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
-
-    def backprop(self, x, y):
-
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-        activation = x
-        activations = [x]
-        zs = [] 
-        for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation)+b
-            zs.append(z)
-            activation = self.sigmoid(z)
-            activations.append(activation)
-
-        delta = self.cost_derivative(activations[-1], y) * self.sigmoid_prime(zs[-1])
-        nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-
-        for l in range(2, self.num_layers):
-            z = zs[-l]
-            sp = self.sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-        return (nabla_b, nabla_w)
-
-    def evaluate(self, test_data):
-        test_results = [(np.argmax(self.feedforward(x)), np.argmax(y)) for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
-
-    def cost_derivative(self, output_activations, y):
-        return (output_activations-y)
-
-    def predict(self, X):
-        data = [x.reshape(-1, 1) for x in X]
-        preds = np.array([np.argmax(self.feedforward(x)) for x in data])
-        return preds
-
 ##############################################################
 # Trees
 ##############################################################
@@ -743,8 +659,88 @@ class kernelRidgeRegression(_kernels):
 
 
 class FeedForward():
-    def __init__(self):
-        pass
+
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def sigmoid(self, z):
+        return 1.0/(1.0+np.exp(-z))
+
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z)*(1-self.sigmoid(z))
+
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = self.sigmoid(np.dot(w, a)+b)
+        return a
+
+    def fit(self, Xtr, ytr, epochs, mini_batch_size, eta, Xva = None, yva = None):
+        training_data = [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(Xtr, ytr)]
+
+        n = len(training_data)
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if Xva is not None and yva is not None:
+                validation_data = [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(Xva, yva)]
+                n_test = len(validation_data)
+                print("Epoch {0} validation acc. {1} / {2}".format(j, self.evaluate(validation_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
+
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x]
+        zs = [] 
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = self.sigmoid(z)
+            activations.append(activation)
+
+        delta = self.cost_derivative(activations[-1], y) * self.sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = self.sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), np.argmax(y)) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations-y)
+
+    def predict(self, X):
+        data = [x.reshape(-1, 1) for x in X]
+        preds = np.array([np.argmax(self.feedforward(x)) for x in data])
+        return preds
     
     
     
